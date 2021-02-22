ac1e54661e4db48bfcf238b71c8250f1d2cb1127
rudizabudi <40042979+rudizabudi@users.noreply.github.com>
2021-02-22 16:42:57 +0100

    v.0.1.1

--- start_gui.bat
+++
@@ -1,4 +0,0 @@
-call %~dp0vEnv\Scripts\activate.bat
-cd %~dp0
-python main.py
-pause
--- tester.py
+++
@@ -1,9 +0,0 @@
-
-
-for i in range(10):
-    try:
-        print(i)
-        if i == 5:
-            raise Exception()
-    except:
-        break
